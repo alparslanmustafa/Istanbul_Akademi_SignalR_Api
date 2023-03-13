6297d10a815a56947fb23cae447e01c1c936226a
alpar <alpar@DESKTOP-VG05C7A>
2023-03-13 12:25:02 +0300

    Add project files.

---
+++ Istanbul_Akademi_SignalR_Api/Controllers/NotificationController.cs
@@ -0,0 +1,27 @@
+﻿using Istanbul_Akademi_SignalR_Api.Hubs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Istanbul_Akademi_SignalR_Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class NotificationController : ControllerBase
+	{
+        private readonly IHubContext<MyHub> _hubContext;
+        public NotificationController(IHubContext<MyHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpGet("{roomCount}")]
+        public async Task<IActionResult> SetRoomCount(int roomCount)
+        {
+            MyHub.RoomCount = roomCount;
+            await _hubContext.Clients.All.SendAsync("Notify", $"Bu oda en fazla {roomCount} kişi alabilir");
+            return Ok();
+        }
+    }
+}
---
+++ Istanbul_Akademi_SignalR_Api/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Istanbul_Akademi_SignalR_Api.Models
+{
+	public class User
+	{
+		public int UserID { get; set; }
+		public string Name { get; set; }
+		public Room Room { get; set; }
+	}
+}
